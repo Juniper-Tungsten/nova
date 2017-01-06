4198cd2b6e82742f69e8a7d8d95420b93c658d14
Jeya ganesh babu J <jjeya@juniper.net>
2017-01-06 00:34:36 -0800

    Fix for python-nova installation failure
    Closes-Bug: #1654500
    Fixed syntax errors.
    Added interpath variable to new functions to fix VM launch failure.
    
    Change-Id: I83ff8c8325349ff03a06859bf21d1c47cad8ce08
    (cherry picked from commit 508778cd179c65995c0d96ffe4578199bc876a9d)

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -3118,7 +3118,7 @@ class LibvirtDriver(driver.ComputeDriver):
         if CONF.libvirt.virt_type == 'uml':
             libvirt_utils.chown(image('disk').path, 'root')
 
-        self._create_and_inject_local_root(context, instance,
+        self._create_and_inject_local_root(context, instance, interpath,
                                            booted_from_volume, suffix,
                                            disk_images, injection_info,
                                            fallback_from_host)
@@ -3207,7 +3207,7 @@ class LibvirtDriver(driver.ComputeDriver):
                                          size=size,
                                          swap_mb=swap_mb)
 
-    def _create_and_inject_local_root(self, context, instance,
+    def _create_and_inject_local_root(self, context, instance, interpath,
                                       booted_from_volume, suffix, disk_images,
                                       injection_info, fallback_from_host):
         # File injection only if needed
@@ -3239,7 +3239,8 @@ class LibvirtDriver(driver.ComputeDriver):
             else:
                 fetch_func = libvirt_utils.fetch_image
             self._try_fetch_image_cache(backend, fetch_func, context,
-                                        root_fname, disk_images['image_id'],
+                                        root_fname, interpath,
+                                        disk_images['image_id'],
                                         instance, size, fallback_from_host)
 
             if need_inject:
