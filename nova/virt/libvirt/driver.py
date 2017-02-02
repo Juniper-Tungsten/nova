0c436f9484c874d4026c717a2c93befe71623405
Jeya ganesh babu J <jjeya@juniper.net>
2017-02-02 12:53:04 -0800

    NFS based live-migration fix
    Closes-Bug: #1655746
    Fix for openstack nfs based live-migration
    Fixed issue related to block-migration
    
    Change-Id: I0322d95e29bd9792c945cd7cda8321f83de0e27d

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -5707,17 +5707,17 @@ class LibvirtDriver(driver.ComputeDriver):
     def _check_shared_storage_test_file(self, filename, instance):
         """Confirms existence of the tmpfile under CONF.instances_path.
         Cannot confirm tmpfile return False."""
-        shared = False
+        dirpath = CONF.instances_path
         if CONF.storage_scope.lower() == "global":
-            shared = True
+            dirpath = os.path.join(CONF.instances_path, "global")
         if len(instance['metadata']) > 0:
             ins_meta = utils.instance_meta(instance)
             for key,value in ins_meta.items():
                 if key.lower() == 'storage_scope':
                     if value.lower() == 'global':
-                        shared = True
+                        dirpath = os.path.join(CONF.instances_path, "global")
                     else:
-                        shared = False
+                        dirpath = CONF.instances_path
                     break
 
         # NOTE(tpatzig): if instances_path is a shared volume that is
@@ -5726,11 +5726,9 @@ class LibvirtDriver(driver.ComputeDriver):
         # just because it takes longer until the client refreshes and new
         # content gets visible.
         # os.utime (like touch) on the directory forces the client to refresh.
-        os.utime(CONF.instances_path, None)
+        os.utime(dirpath, None)
 
-        if not shared:
-            return  False
-        tmp_file = os.path.join(CONF.instances_path, "global", filename)
+        tmp_file = os.path.join(dirpath, filename)
         if not os.path.exists(tmp_file):
             exists = False
         else:
@@ -6666,19 +6664,19 @@ class LibvirtDriver(driver.ComputeDriver):
             elif info['backing_file']:
                 # Creating backing file follows same way as spawning instances.
                 cache_name = os.path.basename(info['backing_file'])
-            if CONF.storage_scope.lower() == "global":
-                interpath = "global"
-            else:
-                interpath = None
-            if len(instance['metadata']) > 0:
-                ins_meta = utils.instance_meta(instance)
-                for key,value in ins_meta.items():
-                    if key.lower() == 'storage_scope':
-                        if value.lower() == 'global':
-                            interpath = "global"
-                        else:
-                            interpath = None
-                        break
+                if CONF.storage_scope.lower() == "global":
+                    interpath = "global"
+                else:
+                    interpath = None
+                if len(instance['metadata']) > 0:
+                    ins_meta = utils.instance_meta(instance)
+                    for key,value in ins_meta.items():
+                        if key.lower() == 'storage_scope':
+                            if value.lower() == 'global':
+                                interpath = "global"
+                            else:
+                                interpath = None
+                            break
 
                 image = self.image_backend.image(instance,
                                                  instance_disk,
